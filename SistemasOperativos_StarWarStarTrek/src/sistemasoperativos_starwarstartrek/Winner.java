2183529165d5d2bd503b26113742224c6aa5dd7d
b-valeria <b.valeria@correo.unimet.edu.ve>
2024-11-21 22:47:25 -0400

    Chore: Clase 5

---
+++ SistemasOperativos_StarWarStarTrek/src/sistemasoperativos_starwarstartrek/Winner.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemasoperativos_starwarstartrek;
+
+/**
+ *
+ * @author valeriabadell
+ */
+public class Winner {
+    private int id;
+    private String name;
+
+    public Winner(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
